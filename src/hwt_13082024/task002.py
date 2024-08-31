484c663fe57ff01691ba72bd24e4fefaa2f6f3f5
Vijaykumar M <vijaykumar.m@se.com>
2024-08-31 22:28:14 +0530

    Home Work Task

---
+++ src/hwt_13082024/task002.py
@@ -0,0 +1,21 @@
+# Task #2
+# Create a program to get num1 to num2 with user input
+# pow num1 to num2
+# Sum, Sub, Mul, Div
+
+num1 = int(input("Enter the number1 : "))
+num2 = int(input("Enter the number2 : "))
+
+result_of_pow = pow(num1, num2)
+print("Power of number1 is :", result_of_pow)
+
+result_of_sum = num1+num2
+result_of_sub = num1-num2
+result_of_mul = num1*num2
+result_of_div = num1/num2
+
+print("Sum of two numbers :", result_of_sum)
+print("Sub of two numbers :", result_of_sub)
+print("Mul of two numbers :", result_of_mul)
+print("Div of two numbers :", result_of_div)
+
