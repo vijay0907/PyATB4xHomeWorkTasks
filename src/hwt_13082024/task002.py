b58d95032f319ace0bb33a69c6b20926437fad23
Vijaykumar M <vijaykumar.m@se.com>
2024-08-31 23:00:22 +0530

    modified task002

--- src/hwt_13082024/task002.py
+++ src/hwt_13082024/task002.py
@@ -7,15 +7,15 @@ num1 = int(input("Enter the number1 : "))
 num2 = int(input("Enter the number2 : "))
 
 result_of_pow = pow(num1, num2)
-print("Power of number1 is :", result_of_pow)
+print(f"Power of number1 is : {result_of_pow}")
 
 result_of_sum = num1+num2
 result_of_sub = num1-num2
 result_of_mul = num1*num2
 result_of_div = num1/num2
 
-print("Sum of two numbers :", result_of_sum)
-print("Sub of two numbers :", result_of_sub)
-print("Mul of two numbers :", result_of_mul)
-print("Div of two numbers :", result_of_div)
 
+print(f"Sum of two numbers : {result_of_sum}")
+print(f"Sub of two numbers : {result_of_sub}")
+print(f"Mul of two numbers : {result_of_mul}")
+print(f"Div of two numbers : {result_of_div}")
